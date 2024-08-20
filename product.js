4cfea2a3e1e3e3f9f4f2adefe05c74fd5d542287
Himangivaghasiya12 <gopivaghasiya001@gmail.com>
2024-08-20 14:22:58 +0530

    lecture_8

--- product.js
+++ product.js
@@ -1,66 +1,37 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const product = require('./product.json')
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+// const express = require('express')
+// const morgan = require('morgan')
+// const { updateUserPro } = require('./controller/product.controller')
+// const app = express()
 
-app.get('/',(req,res) => {
-    res.send('Welcome To Express Server 1412')
-})
+// app.use(morgan('dev'))
+// app.use(express.json())
+// app.use(express.urlencoded({extended:false}))
 
-// CRUD
-// Create Product data
+// app.get('/',(req,res) => {
+//     res.send('Welcome To Express Server 1412')
+// })
 
-app.post('/product' , (req,res) => {
-    // console.log(req.body);
-    product.push(req.body);
-    res.json({message : "Product added Successfully"})
-})
+// // CRUD
+// // Create Product data
 
-// Read Product data - get all products
+// app.post('/product' , addNewUserPro );
 
-app.get('/product' , (req,res) => {
-    res.json(product)
-})
+// // Read Product data - get all products
 
-// Get Single Product
+// app.get('/product' , getAllUsersPro);
 
-app.get('/product/:id' , (req,res) => {
-    let id = +req.params.id;
-    let pro = product.find(item => item.id === id);
-    res.json(pro)
-})
+// // Get Single Product
 
-// Replace Data - put
-app.put('/product/:id' , (req,res) => {
-    let id = +req.params.id;
-    let index = product.findIndex((item) => item.id === id);
-    product.splice(index , 1 , req.body);
-    res.json({message : "Product Replaced Successfully"});
-})
+// app.get('/product/:id' , getUserPro)
 
-// Update data - PATCH
+// // Replace Data - put
+// app.put('/product/:id' , replaceUserPro)
 
-app.patch('/product/:id' , (req,res) => {
-    let id = +req.params.id;
-    let index = product.findIndex((item) => item.id === id);
-    let product2 = product[index];
-    product.splice(index , 1 ,{...product2 , ...req.body});
-    res.json({message : "Product Updated Successfully"});
-})
+// // Update data - PATCH
 
-// Delete Data - DELETE
+// app.patch('/product/:id' , updateUserPro)
 
-app.delete("/product/:id" , (req,res) => {
-    let id = +req.params.id;
-    let index = product.findIndex((item) => item.id === id);
-    product.splice(index , 1);
-    res.json({message: "Product Delete Successfully" });
-})
+// // Delete Data - DELETE
 
-app.listen(1412, () => {
-    console.log("Server Start At http://localhost:1412")
-})
+// app.delete("/product/:id" , deleteUserPro)
