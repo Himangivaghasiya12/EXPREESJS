9dd9f18e9c9d2f93c3ac34612c09c3c9f39e7aae
Himangivaghasiya12 <gopivaghasiya001@gmail.com>
2024-08-20 14:13:32 +0530

    lecture_6

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
-// --------------------------lecture 5----------------------------------------
+// --------------------------lecture 6----------------------------------------
 
 
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const users = require('./friend.json')
+
 // console.log(users)
 
 app.use(morgan('dev'))
@@ -39,6 +39,45 @@ app.get('/user/:id', (req, res) => {
     res.json(item)
 })
 
+// Replace Data - Put
+app.put("/user/:id" , (req,res) => {
+    let id = +req.params.id;
+    let userIndex = users.findIndex((item) => item.id === id);
+    users.splice(userIndex, 1 , req.body);
+    res.json({message: "User Replace Success" });
+})
+
+// Update data - PATCh
+
+app.patch("/user/:id" , (req,res) => {
+    let id = +req.params.id;
+    let userIndex = users.findIndex((item) => item.id === id);
+    let user = users[userIndex];
+    users.splice(userIndex , 1 , {...user , ...req.body});
+    res.json({message: "User Update Success" });
+})
+
+// Delete Data - DELETE
+app.delete("/user/:id" , (req,res) => {
+    let id = +req.params.id;
+    let userIndex = users.findIndex((item) => item.id === id);
+    users.splice(userIndex , 1);
+    res.json({message: "User Delete Success" });
+})
+
 app.listen(1210, () => {
     console.log("Server Start At http://localhost:1210")
 })
+
+
+
+
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.get('/',(req,res) => {
+    res.send('Welcome To Express Server 1210')
+})
+
