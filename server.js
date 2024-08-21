7e5f730810872be2b165c2dcb23fc70550849a67
Himangivaghasiya12 <gopivaghasiya001@gmail.com>
2024-08-21 18:36:24 +0530

    lecture_9

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-// --------------------------lecture 8----------------------------------------
-
-
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
-const mongoose = require("mongoose");
-const userRoutes = require('./routes/user.routes');
-const productRoutes = require('./routes/product.routes');
-// console.log(users)
-
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-app.get('/', (req, res) => {
-    res.send("Welcome To Express Server...")
-})
-
-app.use("/api/user" , userRoutes);
-
-app.use("/api/product" , productRoutes);
-
-app.listen(1210, () => {
-    // Database Connection
-    mongoose
-    .connect('mongodb://localhost:27017/ecommerce')
-    .then(() => console.log("Database connection established  successfully"))
-    .catch(() => console.log(err))
-    console.log("Server Start At http://localhost:1210")
+// --------------------------lecture 9---------------------------------------- 
+require('dotenv').config(); 
+const express = require('express'); 
+const morgan = require('morgan'); 
+const app = express(); 
+const mongoose = require("mongoose"); 
+const port = process.env.PORT; 
+const userRoutes = require('./model/user.model'); 
+ 
+const productRoutes = require('./routes/product.routes'); 
+// console.log(users) 
+ 
+app.use(morgan('dev')) 
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: false })) 
+ 
+app.get('/', (req, res) => { 
+    res.send("Welcome To Express Server...") 
+}) 
+ 
+app.use("/api/user" , userRoutes); 
+ 
+app.use("/api/product" , productRoutes); 
+ 
+app.listen(port, () => { 
+    // Database Connection 
+    mongoose 
+    .connect(process.env.mongo_URL) 
+    .then(() => { 
+    console.log("Database connection established  successfully") 
+    }) 
+    .catch((err) => console.log(err)) 
+    console.log(`Server Start At http://localhost:${port}`) 
 })
